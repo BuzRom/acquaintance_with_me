826ac7adf28ba8d6c0b1711206d64dd9f4ff9b14
BuzRom <romanbuzukov@gmail.com>
2021-10-19 22:44:40 +0300

    update

--- js/script.js
+++ js/script.js
@@ -234,6 +234,7 @@ form.addEventListener('submit', e => {
    form.message.value = '';
 });
 
+
 // flash-light
 const about = document.documentElement;
 
@@ -241,3 +242,76 @@ about.addEventListener('mousemove', e => {
    about.style.setProperty('--x', e.clientX + 'px')
    about.style.setProperty('--y', e.clientY - 150 + 'px')
 })
+
+
+//hover text
+class HoverButton {
+   constructor(el) {
+      this.el = el;
+      this.hover = false;
+      this.calculatePosition();
+      this.attachEventsListener();
+   }
+
+   attachEventsListener() {
+      window.addEventListener('mousemove', e => this.onMouseMove(e));
+      window.addEventListener('resize', e => this.calculatePosition(e));
+   }
+
+   calculatePosition() {
+      gsap.set(this.el, {
+         x: 0,
+         y: 0,
+         scale: 1
+      });
+      const box = this.el.getBoundingClientRect();
+      this.x = box.left + (box.width * 0.5);
+      this.y = box.top + (box.height * 0.5);
+      this.width = box.width;
+      this.height = box.height;
+   }
+
+   onMouseMove(e) {
+      let hover = false;
+      let hoverArea = (this.hover ? 0.7 : 0.5);
+      let x = e.clientX - this.x;
+      let y = e.clientY - this.y;
+      let distance = Math.sqrt(x * x + y * y);
+      if (distance < (this.width * hoverArea)) {
+         hover = true;
+         if (!this.hover) {
+            this.hover = true;
+         }
+         this.onHover(e.clientX, e.clientY);
+      }
+
+      if (!hover && this.hover) {
+         this.onLeave();
+         this.hover = false;
+      }
+   }
+
+   onHover(x, y) {
+      gsap.to(this.el, {
+         x: (x - this.x) * 0.4,
+         y: (y - this.y) * 0.4,
+         scale: 1.15,
+         ease: 'power2.out',
+         duration: 0.4
+      });
+      this.el.style.zIndex = 10;
+   }
+   onLeave() {
+      gsap.to(this.el, {
+         x: 0,
+         y: 0,
+         scale: 1,
+         ease: 'elastic.out(1.2, 0.4)',
+         duration: 0.7
+      });
+      this.el.style.zIndex = 1;
+   }
+}
+
+const text = document.querySelector('.hero__text');
+new HoverButton(text);
