18ebe520c26a41ca3012c5b25d2878ae33567570
BuzRom <romanbuzukov@gmail.com>
2021-10-19 18:05:30 +0300

    new design

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# [Demo](https://buzrom.github.io/acquaintance_with_me/)
+## [Demo](https://buzrom.github.io/acquaintance_with_me/)
+
+### Desine: [Figma](https://www.figma.com/file/Qlk9Y4RycygcYuucGolW9C/Free--Landing--Page-Template-(Copy)?node-id=0%3A1)
 ![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/rainbow.png)
--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 'use strict'
 
 //mobile or pc
-const isMobile = {
-   Android: () => {
-      return navigator.userAgent.match(/Android/i);
-   },
-   BlackBerry: () => {
-      return navigator.userAgent.match(/BlackBerry/i);
-   },
-   iOS: () => {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-   },
-   Opera: () => {
-      return navigator.userAgent.match(/Opera Mini/i);
-   },
-   Windows: () => {
-      return navigator.userAgent.match(/IEMobile/i);
-   },
-   any: () => {
-      return (
-         isMobile.Android() ||
-         isMobile.BlackBerry() ||
-         isMobile.iOS() ||
-         isMobile.Opera() ||
-         isMobile.Windows()
-      );
-   }
-};
-if (isMobile.any()) {
-   document.body.classList.add('_touch');
-}
-else {
-   document.body.classList.add('_pc');
-};
+// const isMobile = {
+//    Android: () => {
+//       return navigator.userAgent.match(/Android/i);
+//    },
+//    BlackBerry: () => {
+//       return navigator.userAgent.match(/BlackBerry/i);
+//    },
+//    iOS: () => {
+//       return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+//    },
+//    Opera: () => {
+//       return navigator.userAgent.match(/Opera Mini/i);
+//    },
+//    Windows: () => {
+//       return navigator.userAgent.match(/IEMobile/i);
+//    },
+//    any: () => {
+//       return (
+//          isMobile.Android() ||
+//          isMobile.BlackBerry() ||
+//          isMobile.iOS() ||
+//          isMobile.Opera() ||
+//          isMobile.Windows()
+//       );
+//    }
+// };
+// if (isMobile.any()) {
+//    document.body.classList.add('_touch');
+// }
+// else {
+//    document.body.classList.add('_pc');
+// };
 
 
 //menu burger
@@ -234,7 +234,7 @@ form.addEventListener('submit', e => {
    form.message.value = '';
 });
 
-//flash-light
+// flash-light
 const about = document.documentElement;
 
 about.addEventListener('mousemove', e => {
